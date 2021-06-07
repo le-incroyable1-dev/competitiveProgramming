89f28b748a03402a57ffbd66cb69aa6170f7b902
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-06-07 16:29:39 +0530

    Update COCONUT.cpp

--- codechef long challenge/june 2021/COCONUT.cpp
+++ codechef long challenge/june 2021/COCONUT.cpp
@@ -1,5 +1,8 @@
 /*
-Chefland has 2 different types of coconut, type A and type B. Type A contains only xa milliliters of coconut water and type B contains only xb grams of coconut pulp. Chef's nutritionist has advised him to consume Xa milliliters of coconut water and Xb grams of coconut pulp every week in the summer. Find the total number of coconuts (type A + type B) that Chef should buy each week to keep himself active in the hot weather.
+Chefland has 2 different types of coconut, type A and type B. Type A contains only xa milliliters of coconut water and 
+type B contains only xb grams of coconut pulp. Chef's nutritionist has advised him to consume Xa milliliters of coconut 
+water and Xb grams of coconut pulp every week in the summer. Find the total number of coconuts (type A + type B) that 
+Chef should buy each week to keep himself active in the hot weather.
 
 Input
 The first line contains an integer T, the number of test cases. Then the test cases follow.
