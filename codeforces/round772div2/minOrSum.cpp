9201be39554269cb4e2372c152937c1223eff735
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2022-02-24 20:07:33 +0530

    Update minOrSum.cpp

--- codeforces/round772div2/minOrSum.cpp
+++ codeforces/round772div2/minOrSum.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+/*
+The answer is a1|a2|⋯|an. Here is the proof:
+
+Let m=a1|a2|⋯|an. After an operation, the value m won't change.
+
+Since, a1|a2|⋯|an ≤ a1+a2+⋯+an, the sum of the array has a lower bound of m. 
+And this sum can be constructed easily: for all i∈[1,n−1], set ai+1 to ai|ai+1 and ai to 0.
+*/
 
 int main () {
 
