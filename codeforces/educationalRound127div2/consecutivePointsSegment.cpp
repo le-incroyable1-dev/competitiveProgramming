8112ea87c36706bda81d360344e37916fd2ed216
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2022-04-22 22:05:44 +0530

    Add files via upload

---
+++ codeforces/educationalRound127div2/consecutivePointsSegment.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=ab;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define itr(type) type::iterator itr
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+
+//INCOMPLETE
+void sol(vi v, ll n){
+
+    if(n < 2){
+        cout << "YES" << endl;
+        return;
+    }
+
+    vpi vn;
+
+    sort(all(v));
+
+    fr(i, n)
+    vn.pb(mp(v.at(i), 0));
+
+    fr(i, n-1){
+        pii p1 = vn.at(i);
+        pii p2 = vn.at(i+1);
+
+        ll n1 = p1.first;
+        ll n2 = p2.first;
+
+        if(p1.second == 0){
+            if(n2 - n1 == 3){
+                p1.first++;
+                p2.first++;
+                p1.second++;
+                p2.second++;
+            }
+            else if(n2 - n1 == 2){
+
+            }
+        }
+        else{
+
+        }
+    }
+}
+
+
+
+int main(void){
+    
+    fast;
+
+    test(t){
+
+        ll n;
+
+        ll ele;
+        vi v;
+
+        fr(i, n){
+            cin >> ele;
+            v.pb(ele);
+        }
+
+        sol(v, n);
+    }
+    
+}
