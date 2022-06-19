d39595d1530925fb846e5b17f47b07add90308d2
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2022-06-20 01:15:17 +0530

    Update wtOfTheSystemOfNestedSegments.cpp

--- codeforces/round776div3/wtOfTheSystemOfNestedSegments.cpp
+++ codeforces/round776div3/wtOfTheSystemOfNestedSegments.cpp
@@ -1,48 +1,89 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define itr(type) type::iterator itr
+//#define tn TreeNode*
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
 
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-
-struct point{
-    int weight, position, id;
-};
-
-void solve(){
-    int n, m;
-    cin >> n >> m;
-    vector<point>points(m);
-
-    forn(i, m) {
-        cin >>  points[i].position >> points[i].weight;
-        points[i].id = i + 1;
-    }
-
-    sort(points.begin(), points.end(), [] (point a, point b){
-        return a.weight < b.weight;
-    });
-
-    int sum = 0;
-    forn(i, m){
-        if(i < 2 * n) sum += points[i].weight;
-        else points.pop_back();
-    }
-
-    sort(points.begin(), points.end(), [] (point a, point b){
-        return a.position < b.position;
-    });
-
-    cout << sum << endl;
-    forn(i, n){
-        cout << points[i].id << ' ' << points[2 * n - i - 1].id << endl;
-    }
+bool compare1(pii &p1, pii &p2){
+    return p1.sec < p2.sec;
 }
 
-int main() {
-    int t;
-    cin >> t;
-    while(t--){
-        solve();
+bool compare2(pii &p1, pii &p2){
+    return p1.fi < p2.fi;
+}
+
+void sol(ll n, ll m , vpi v, unordered_map<ll,ll> mapp){
+
+    sort(v.begin(), v.end(), compare1);
+
+    vpi pts;
+    ll mwt = 0;
+    fr(i, 2*n){
+        mwt += v[i].sec;
+        pts.pb(v[i]);
     }
-    return 0;
+
+    sort(pts.begin(), pts.end(), compare2);
+
+    cout << mwt << endl;
+    fr(i, n){
+        cout << mapp.find(pts[i].fi)->sec << " " << mapp.find(pts[2*n-1-i].fi)->sec << endl;
+    }
+
+    return;
+}
+
+
+
+int main(void){
+    
+    fast;
+
+    test(t){
+
+        ll n,m;
+        cin >> n >> m;
+
+        vpi v;
+        pii p;
+
+        ll x, w;
+
+        unordered_map<ll, ll> mapp;
+
+        fr(i, m){
+            cin >> x >> w;
+            p.fi = x;
+            p.sec = w;
+            mapp.insert(mp(x, i+1));
+            v.pb(p);
+        }
+
+        sol(n, m, v, mapp);
+    }
+    
 }
