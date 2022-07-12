0e435c260d05c3c7f4aa1519ffa70f4638d13f9c
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2022-07-12 22:33:52 +0530

    Update countTheNumberOfIdealArrays.cpp

--- leetcode/weekly301/countTheNumberOfIdealArrays.cpp
+++ leetcode/weekly301/countTheNumberOfIdealArrays.cpp
@@ -28,6 +28,8 @@ typedef long double  ld;
 //#define tn TreeNode*
 long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
 
+
+//DP  : https://leetcode.com/problems/count-the-number-of-ideal-arrays/discuss/2261491/C%2B%2B-Accepted-or-DP-or-O(15*n)-space-or-Complete-Explanation
 //recursive, TLE
 class Solution {
 public:
