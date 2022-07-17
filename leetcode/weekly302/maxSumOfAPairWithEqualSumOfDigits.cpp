e25d63bcdc2b0a0558a3001ba2140fad0ce79900
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2022-07-17 09:34:34 +0530

    Create maxSumOfAPairWithEqualSumOfDigits.cpp

---
+++ leetcode/weekly302/maxSumOfAPairWithEqualSumOfDigits.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define itr(type) type::iterator itr
+//#define tn TreeNode*
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        ll n = nums.size();
+
+
+        ll ans = -1;
+
+        if(n == 1)
+        return -1;
+
+        sort(all(nums));
+        reverse(all(nums));
+
+        unordered_map<int, vector<int>> m;
+        unordered_map<int, vector<int>>::iterator itr;
+
+        fr(i, n){
+            string num = to_string(nums[i]);
+            int sum = 0;
+            ll sz = num.length();
+            fr(i, sz){
+                sum += stoi(string(1, num[i]));
+            }
+            itr = m.find(sum);
+
+            if(itr == m.end()){
+                vector<int> v;
+                v.pb(nums[i]);
+                m.insert(mp(sum, v));
+            }
+            else
+            itr->second.pb(nums[i]);
+        }
+        
+
+        itr = m.begin();
+
+        while(itr != m.end()){
+            
+            if(itr->second.size() < 2){
+                itr++;
+                continue;
+            }
+            
+            ll val = itr->second.at(0) + itr->second.at(1);
+            
+            if(val > ans)
+            ans = val;
+
+            itr++;
+        }
+
+        if(ans > INT_MAX)
+            return INT_MAX;
+        else
+            return (int)ans;
+
+    }
+};
