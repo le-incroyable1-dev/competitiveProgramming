bfc42e8b3f034cf91387e97c3af512ea62629771
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2022-09-21 18:32:23 +0530

    Create longestNiceSubarray.cpp

---
+++ leetcode/longestNiceSubarray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        
+        int max_len = 1;
+        int res = 0;
+        
+        for(int i = 0; i < n; i++){
+            while((res & nums[i]) != 0)
+                res ^= nums[j++];
+            
+            res |= nums[i];
+            max_len = max(max_len, i-j+1);
+        }
+        
+        return max_len;
+        
+    }
+};
