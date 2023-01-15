0453fc9b5f9bd4e9e8dbf98a497b37fc64fc9b39
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-01-15 23:15:54 +0530

    Create 2536.cpp

---
+++ leetcode/2536.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> ans(n, vector<int>(n, 0));
+
+        //sweep-line
+        for(auto q : queries){
+            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+
+            for(int r = r1; r <= r2; ++r){
+                ans[r][c1] += 1;
+                
+                if(c2 + 1 < n)
+                ans[r][c2+1] -= 1;
+            }
+        }
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 1; j < n; ++j){
+                ans[i][j] += ans[i][j-1];
+            }
+        }
+
+        return ans;
+    }
+};
