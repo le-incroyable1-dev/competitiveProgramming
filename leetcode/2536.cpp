1bcb0da3119c834be49a2bf5806eabba218dfff0
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-01-16 20:29:22 +0530

    Update 2536.cpp

--- leetcode/2536.cpp
+++ leetcode/2536.cpp
@@ -9,15 +9,19 @@ public:
         //sweep-line
         for(auto q : queries){
             int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
-
+            
+            //for every row, mark +1 where the submatrix starts
+            //then mark -1 after it ends (according to the column)
             for(int r = r1; r <= r2; ++r){
                 ans[r][c1] += 1;
+                
                 
                 if(c2 + 1 < n)
                 ans[r][c2+1] -= 1;
             }
         }
 
+        //take the sum across the rows to get the final sweeped result
         for(int i = 0; i < n; ++i){
             for(int j = 1; j < n; ++j){
                 ans[i][j] += ans[i][j-1];
