8841bed0cb492f4d53e1705f93bce01b98799d0f
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-01-29 15:32:35 +0530

    Create 146.cpp

---
+++ leetcode/146.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class LRUCache {
+private:
+    int capacity;
+    list<int> recent;
+    unordered_map<int, int> cache;
+    unordered_map<int, list<int>::iterator> pos;
+    void use(int key) {
+        if (pos.find(key) != pos.end()) {
+            recent.erase(pos[key]);
+        } else if (recent.size() >= capacity) {
+            int old = recent.back();
+            recent.pop_back();
+            cache.erase(old);
+            pos.erase(old);
+        }
+        recent.push_front(key);
+        pos[key] = recent.begin();
+    }
+public:
+    LRUCache(int capacity): capacity(capacity) {}
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            use(key);
+            return cache[key];
+        }
+        return -1;
+    }
+    void put(int key, int value) {
+        use(key);
+        cache[key] = value;
+    }
+};
+
+int main(){
+    LRUCache lRUCache = LRUCache(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1);    // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2);    // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1);    // return -1 (not found)
+    lRUCache.get(3);    // return 3
+    lRUCache.get(4);    // return 4
+    return 0;
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
