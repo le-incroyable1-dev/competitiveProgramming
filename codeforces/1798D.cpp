ea9ee1c3d3f138fbcfc4f7d4f1f7258a7b31953e
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-03-27 22:46:06 +0530

    Create 1798D.cpp

---
+++ codeforces/1798D.cpp
@@ -0,0 +1,106 @@
+//author : aurav
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define tn TreeNode*
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+
+
+void sol(){
+
+    ll sz; cin >> sz;
+
+    if(sz == 1){
+        int ele; cin >> ele;
+        cout << "NO\n"; return;
+    }
+    vector<ll> a;
+    ll ele;
+    fr(i, sz) cin >> ele, a.pb(ele);
+    
+    if(*max_element(all(a)) == 0) {
+        cout << "NO\n"; return;
+    }
+    
+
+    /////////////
+
+    vi pa, na;
+
+    fr(i, sz){
+        if(a[i] >= 0) pa.pb(a[i]);
+        else na.pb(a[i]);
+    }
+
+    sort(all(pa));
+    sort(all(na));
+    reverse(all(na));
+
+    ll i = 0, j = 0;
+
+    ll n = pa.size();
+    ll m = na.size();
+
+    vector<ll> myv;
+    ll sum = 0;
+
+    while(i < n || j < m){
+        if(sum < 0){
+            sum += pa[i];
+            myv.pb(pa[i]); ++i;
+        }
+        else{
+
+            if(j == m && sum == 0){
+                while(i < n) myv.pb(pa[i]), ++i;
+                break;
+            }
+
+            sum += na[j];
+            myv.pb(na[j]); ++j;
+        }
+    }
+
+    
+
+    cout << "Yes\n";
+    for(auto val : myv) cout << val << " ";
+    cout << "\n"; return;
+    
+}
+
+
+
+int main(void){
+
+    fast;
+
+    test(t){
+        sol();
+    } 
+
+    
+}
