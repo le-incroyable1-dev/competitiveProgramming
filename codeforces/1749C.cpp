453346856f80742cb2901dcd0505e8123b477ba0
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-04-07 16:21:41 +0530

    Create 1749C.cpp

---
+++ codeforces/1749C.cpp
@@ -0,0 +1,82 @@
+//author : aurav
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define tn TreeNode*
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+
+
+void sol(){
+
+    ll n; cin >> n;
+    vi a; ll ele; fr(i, n) cin >> ele, a.pb(ele);
+
+    sort(all(a));
+
+    if(a[0]!=1){
+        cout << 0 << "\n";
+        return;
+    }
+
+    multiset<int> s;
+    ll ans = 0;
+
+    for(int k = 1; k <= n; ++k){
+        s.clear();
+        for(auto val : a) s.insert(val);
+
+        fr(i, k){
+            multiset<int>::iterator itr = s.upper_bound(k-i);
+            if(itr == s.begin()) break;
+
+            itr--;
+            s.erase(itr);
+            if(!s.empty()){
+                s.insert(*s.begin() + k - i);
+                s.erase(s.begin());
+            }
+        }
+
+        if(n-s.size() == k) ans = k;
+        else break;
+        
+    }
+
+    cout << ans << "\n"; return;
+}
+
+
+
+int main(void){
+
+    fast;
+
+    test(t){
+        sol();
+    } 
+
+    
+}
