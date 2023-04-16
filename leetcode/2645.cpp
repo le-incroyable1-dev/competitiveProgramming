ee27df2076055b51080641be684078208d4f1697
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-04-16 10:13:03 +0530

    Create 2645.cpp

---
+++ leetcode/2645.cpp
@@ -0,0 +1,85 @@
+//author : aurav
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define tn TreeNode*
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+
+
+class Solution {
+public:    
+  
+    int addMinimum(string w) {
+        int n = w.length();
+        int ans = 0;
+        int i = 0;
+        while(i < n){
+            char c = w[i];
+            if(c == 'a'){
+                if(i+1 >= n) ans += 2;
+                else{
+                    if(w[i+1] == 'b'){
+                        ++i;
+                        if(i+1 >= n) ans += 1;
+                        else{
+                            if(w[i+1] == 'c'){
+                                ++i;
+                            }
+                            else{
+                                ans++;
+                            }
+                        }
+                    }
+                    else{
+                        if(w[i+1] == 'c'){
+                            ans += 1;
+                            ++i;
+                        }
+                        else{
+                            ans += 2;
+                        }
+                    }
+                }
+            }
+            else if(c == 'b'){
+                ans += 1;
+
+                if(i+1 >= n) ++ans;
+                else{
+                    if(w[i+1] == 'c') ++i;
+                    else ans += 1;
+                }
+            }
+            else{
+                ans += 2;
+            }
+
+            ++i;
+        }
+
+        return ans;
+    }
+};
