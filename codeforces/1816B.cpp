a36a9ec7cea82d8f4cd3f59a2b3e38451b3102bf
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2023-05-11 19:48:43 +0530

    Create 1816B.cpp

---
+++ codeforces/1816B.cpp
@@ -0,0 +1,61 @@
+//author : aurav
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+//#define ln ListNode*
+//#define tn TreeNode*
+long long powerof2[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+
+
+void sol(){
+    ll n; cin >> n;
+    vector<ll> r1(n);
+    vector<ll> r2(n);
+
+    ll c = 1;
+    ll m = (2*n);
+
+    r1[0] = m;
+    r2[n-1] = m-1;
+
+    for(int i = 1; i < n; i += 2) { r1[i] = c; ++c; r2[i-1] = c; ++c;}
+    for(int i = n-2; i > 0; i -= 2) { r1[i] = c; ++c; r2[i-1] = c; ++c;}
+
+    for(auto val : r1) cout << val << " ";
+    cout << "\n";
+    for(auto val : r2) cout << val << " ";
+    cout << "\n";
+
+    return;
+}
+
+
+
+int main(void){
+    fast;
+    test(t){
+        sol();
+    } 
+}
